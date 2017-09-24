7c1a2fd94139d197a7d41c710a07de0efddbafff
T-Dex <dex.m.mills@gmail.com>
2017-09-24 04:08:41 -0600

    Add firebase and yarn lock file

--- src/App.js
+++ src/App.js
@@ -3,6 +3,16 @@ import firebase from 'firebase';
 import Rebase from 're-base';
 import './styles/App.css';
 
+const app= firebase.initializeApp({
+   apiKey: "AIzaSyDkgAmOpumXz2EYAr8DKIdX4L0vIepQF-s",
+   authDomain: "myf-ingwebsite.firebaseapp.com",
+   databaseURL: "https://myf-ingwebsite.firebaseio.com",
+   storageBucket: "myf-ingwebsite.appspot.com",
+ })
+
+const base = Rebase.createClass(app.database());
+
+
 class App extends Component {
   render() {
     return (
