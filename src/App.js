5e6175b45c3574c223e7fb8d46baf5a84f44b25e
T-Dex <dex.m.mills@gmail.com>
2017-09-24 04:01:58 -0600

    Base files added

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import firebase from 'firebase';
+import Rebase from 're-base';
+import './styles/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+
+
+      </div>
+    );
+  }
+}
+
+export default App;
